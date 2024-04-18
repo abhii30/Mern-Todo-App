1c51470892da98f322023a5ec249222d3dabc6fa
abhii30 <akumar300599@gmail.com>
2024-04-18 07:52:43 +0530

    fixed css

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>WELCOME TO MY Todo App</h1>
+      <p>
+        Created By <span>Abhishek</span>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
