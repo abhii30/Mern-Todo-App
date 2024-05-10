87d73b2854cc14c8e4bd16916455698c58432b3a
abhii30 <akumar300599@gmail.com>
2024-05-10 13:00:27 +0530

    reset password

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,162 +1,183 @@
-const userModel = require('../model/user.model.js');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const todoListModel = require('../model/todo-list.model.js');
+const userModel = require("../model/user.model.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const todoListModel = require("../model/todo-list.model.js");
 
 // signup
 exports.signup = async (req, res) => {
-    const { name, email, password } = req.body;
-    try {
+  const { name, email, password } = req.body;
+  try {
+    // hash the password
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    // save the user
+    const userInfo = userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await userInfo.save();
 
-        // hash the password
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        // save the user
-        const userInfo = userModel({
-            name,
-            email,
-            password: hashedPassword
-        });
-        await userInfo.save();
+    // Generate JWT token
+    const token = jwt.sign({ userId: userInfo._id }, process.env.SECRET, {
+      expiresIn: "12h",
+    });
 
-        // Generate JWT token
-        const token = jwt.sign({ userId: userInfo._id }, process.env.SECRET, {
-            expiresIn: '12h'
-        });
-
-        // Set the JWT token as a cookie
-        res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none' });
-
-        // Send the status and success message
-        return res.status(200).json({
-            success: true,
-            message: 'User signup successful',
-            token: token
-        });
-    } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        });
-    }
-};
-
-
-// login
-exports.login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await userModel.findOne({ email });
-    try {
-
-        // Generate JWT token
-        const token = jwt.sign({ userId: user._id }, process.env.SECRET, {
-            expiresIn: '12h'
-        });
-
-        // Set the JWT token as a cookie
-        res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none' });
-
-        // Send the status and success message
-        return res.status(200).json({
-            success: true,
-            message: 'User login successfulyl',
-            token: token
-        });
-    } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        });
-    }
-};
-
-// logout
-exports.logout = async (req, res) => {
-    // Clear the JWT token cookie
-    res.clearCookie('token');
+    // Set the JWT token as a cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
     // Send the status and success message
     return res.status(200).json({
-        success: true,
-        message: 'User logout successfully'
+      success: true,
+      message: "User signup successful",
+      token: token,
     });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
+// login
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  try {
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET, {
+      expiresIn: "12h",
+    });
+
+    // Set the JWT token as a cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    // Send the status and success message
+    return res.status(200).json({
+      success: true,
+      message: "User login successfulyl",
+      token: token,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  // get user based on email
+  const user = await userModel.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "There is no user with email address",
+    });
+  }
+  // generate the random reset token
+
+  // send it to user's email
+};
+
+exports.resetPassword = async (req, res) => {};
+
+// logout
+exports.logout = async (req, res) => {
+  // Clear the JWT token cookie
+  res.clearCookie("token");
+
+  // Send the status and success message
+  return res.status(200).json({
+    success: true,
+    message: "User logout successfully",
+  });
+};
 
 // get user
 exports.getUser = async (req, res) => {
-    const { userId } = req.user;
+  const { userId } = req.user;
 
-    try {
-        // find the user by ID
-        const user = await userModel.findById(userId);
+  try {
+    // find the user by ID
+    const user = await userModel.findById(userId);
 
-        // check if user exists
-        if (!user) {
-            return res.status(404).json({
-                success: false,
-                message: 'User not found'
-            });
-        }
-
-        // send the user information
-        return res.status(200).json({
-            success: true,
-            user
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        });
+    // check if user exists
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
     }
+
+    // send the user information
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // update user
 exports.userUpdate = async (req, res) => {
-    const { name, email } = req.body;
-    const userId = req.user.userId; // retrieved from the JWT token
+  const { name, email } = req.body;
+  const userId = req.user.userId; // retrieved from the JWT token
 
-    try {
-        // update the user
-        await userModel.findByIdAndUpdate(userId, { name, email });
+  try {
+    // update the user
+    await userModel.findByIdAndUpdate(userId, { name, email });
 
-        // send the status and success message
-        return res.status(200).json({
-            success: true,
-            message: 'User updated successfully'
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        });
-    }
+    // send the status and success message
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // delete user
 exports.userDelete = async (req, res) => {
-    const userId = req.user.userId; // retrieved from the JWT token
+  const userId = req.user.userId; // retrieved from the JWT token
 
-    try {
-        // Delete the user and their associated todos
-        await userModel.findByIdAndDelete(userId);
-        await todoListModel.deleteMany({ userId });
+  try {
+    // Delete the user and their associated todos
+    await userModel.findByIdAndDelete(userId);
+    await todoListModel.deleteMany({ userId });
 
-        // Clear the cookies
-        res.clearCookie("token");
+    // Clear the cookies
+    res.clearCookie("token");
 
-        // Send the status and success message
-        return res.status(200).json({
-            success: true,
-            message: "User and associated todos deleted successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({
-            success: false,
-            message: error.message,
-        });
-    }
+    // Send the status and success message
+    return res.status(200).json({
+      success: true,
+      message: "User and associated todos deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
